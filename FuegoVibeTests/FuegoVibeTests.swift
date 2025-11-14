4b4bb500bc7833c62673e8dc2a9230a09a7a9ba6
Swiren6 <118105838+Swiren6@users.noreply.github.com>
2025-11-14 13:09:08 +0100

    Initial Commit

---
+++ FuegoVibeTests/FuegoVibeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FuegoVibeTests.swift
+//  FuegoVibeTests
+//
+//  Created by mac on 14/11/2025.
+//
+
+import Testing
+@testable import FuegoVibe
+
+struct FuegoVibeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
